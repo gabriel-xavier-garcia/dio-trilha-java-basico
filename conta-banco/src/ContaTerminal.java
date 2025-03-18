54c5a96e5a725e9a19324650f0f51d68d918e446
Gabriel Xavier Garcia <gabrielproaj@gmail.com>
2025-03-18 00:17:42 -0300

    Informações adquiridas do usuário e atribuida ao construtor.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Prezado cliente, seja bem vindo ao nosso banco! ");
+        System.out.println();
+        System.out.print("Informe o número da conta: ");
+        int numero = sc.nextInt();
+        System.out.print("Informe o número da agencia: ");
+        String agencia = sc.next();
+        System.out.print("Informe o seu nome: ");
+        sc.nextLine();
+        String nomeCliente = sc.nextLine();
+        System.out.print("Informe o valor para inicializar sua conta: ");
+        double saldo = sc.nextDouble();
+
+        Usuario usuario = new Usuario(numero, agencia, nomeCliente, saldo);
+
+        System.out.println(usuario);
+        
+
+
+
+        sc.close();
+
     }
 }
